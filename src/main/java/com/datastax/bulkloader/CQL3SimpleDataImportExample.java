a0c4c8009821f0b0bbe14d5c649e11865f511cd5
Patrick McFadin <pmcfadin@gmail.com>
2013-05-27 10:23:33 -0700

    Added a bunch more comments for clarity

--- src/main/java/com/datastax/bulkloader/CQL3SimpleDataImportExample.java
+++ src/main/java/com/datastax/bulkloader/CQL3SimpleDataImportExample.java
@@ -16,9 +16,60 @@ import org.apache.cassandra.db.marshal.UTF8Type;
 import org.apache.cassandra.dht.Murmur3Partitioner;
 import org.apache.cassandra.io.sstable.SSTableSimpleUnsortedWriter;
 
+/*
+ * A very simple example of creating CQL3 compatible SSTables for use with bulk loader.
+ * 
+ * To use this example, you need to create a keyspace called 'test'
+ * 
+ * From cqlsh run the following:
+ * 
+ * CREATE KEYSPACE test WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };
+ * 
+ * use test;
+ * 
+ * create table test (
+ * 	a varchar,
+ * 	b int,
+ * 	c varchar,
+ * 	primary key (a,b)
+ * );
+ * 
+ * The code below will create data files that simulate the following CQL commands:
+ * 
+ * insert into test(a,b,c) values ('one',1,'one');
+ * insert into test(a,b,c) values ('two',2,'two');
+ * insert into test(a,b,c) values ('three',3,'three');
+ * 
+ * If you are learning how this works, it would be a good idea to look at how the data 
+ * is stored using the cassandra-cli. Insert the above values using cqlsh, then do the 
+ * following from cassandra-cli:
+ * 
+ * use test;
+ * list test;
+ * 
+ * You will see this output:
+ * 
+ * Using default limit of 100
+ * Using default column limit of 100
+ * -------------------
+ * RowKey: three
+ * => (column=3:, value=, timestamp=1369671593542000)
+ * => (column=3:c, value=7468726565, timestamp=1369671593542000)
+ * -------------------
+ * RowKey: one
+ * => (column=1:, value=, timestamp=1369671593542000)
+ * => (column=1:c, value=6f6e65, timestamp=1369671593542000)
+ * -------------------
+ * RowKey: two
+ * => (column=2:, value=, timestamp=1369671593542000)
+ * => (column=2:c, value=74776f, timestamp=1369671593542000)
+ * 
+ * 3 Rows Returned.
+ * 
+ * Use this output to see how it relates to the way we write data below.
+ * 
+*/
 public class CQL3SimpleDataImportExample {
-
-	static String filename;
 
 	public static void main(String[] args) throws IOException {
 
@@ -27,7 +78,15 @@ public class CQL3SimpleDataImportExample {
 		if (!directory.exists())
 			directory.mkdir();
 
-		// Create a composite column with the types for each part
+		/* 
+		 * Create a composite column with the types for each part
+		 * 
+		 * Each column will have the types in order of the table creation command
+		 * In this case we have an int then a varchar(UTF8 is the underlying type)
+		 * Use the type mapping here: http://www.datastax.com/docs/1.2/cql_cli/cql_data_types
+		 *
+		*/
+		
 		List<AbstractType<?>> compositeColumnValues = new ArrayList<AbstractType<?>>();
 		compositeColumnValues.add(IntegerType.instance);
 		compositeColumnValues.add(UTF8Type.instance);
@@ -41,6 +100,7 @@ public class CQL3SimpleDataImportExample {
 		// Create a single timestamp for each insert
 		long timestamp = System.currentTimeMillis() * 1000;
 
+		// First row. RowKey = a ='one', b=1, c='one'
 		Builder builder = compositeColumn.builder();
 
 		testWriter.newRow(ByteBuffer.wrap("one".getBytes()));
@@ -58,7 +118,7 @@ public class CQL3SimpleDataImportExample {
 		testWriter.addColumn(builder.build(), bytes("one"), timestamp);
 
 		
-		// Next row
+		// Next row. RowKey = a ='two', b=2, c='two'
 		builder = compositeColumn.builder();
 
 		testWriter.newRow(ByteBuffer.wrap("two".getBytes()));
@@ -76,7 +136,7 @@ public class CQL3SimpleDataImportExample {
 		testWriter.addColumn(builder.build(), bytes("two"), timestamp);
 		
 		
-		// Next row
+		// Next row. RowKey = a ='three', b=3, c='three'
 		builder = compositeColumn.builder();
 
 		testWriter.newRow(ByteBuffer.wrap("three".getBytes()));
